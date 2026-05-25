d1f0c7c0db0df337d6a71ed869af3373abc3fc2a
João Vitor <engs-joaokutianski@camporeal.edu.br>
2026-05-25 20:23:26 -0300

    aaaa

---
+++ src/components/Titulo.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1>O projeto em Vue.js</h1>
+        <p>Primeiro <strong>projeto</strong> criado para testes</p>
+    </header>
+</template>
+
+<style>
+    header{
+        background: rgb(7, 61, 0);
+        color: rgb(255, 255, 255);
+        padding: 20px;
+        border-radius: 10px;
+    }
+</style>
